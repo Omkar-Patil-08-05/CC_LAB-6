4e46e162db606e8b116682768a525ff5d8a7be3d
Omkar Patil <08omkarpatil05@gmail.com>
2026-02-26 15:09:46 +0530

    Converted C++ backend to HTTP server

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 int main() {
-    std::cout << "Lab 6 Backend Running Successfully!" << std::endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 5);
+
+    while (true) {
+        int client = accept(server_fd, NULL, NULL);
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Served by backend: container\n";
+
+        send(client, response.c_str(), response.length(), 0);
+        close(client);
+    }
+
     return 0;
 }
